962c7bc9f9577bbe6908bcdc2fdfc290fdcc10dd
YellowAut <52257177+YellowAut@users.noreply.github.com>
2021-12-16 20:54:37 +0100

    Add files via upload

--- program0.0/program.ino
+++ program0.0/program.ino
@@ -14,35 +14,21 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
-  //Serial.println("setup");
   Serial.begin(9600);
   //Priprava LCDcka
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,1);
-  Serial.println("Zadej pocet vterin: ");
-  lcd.print("Zadej pocet vterin: ");
-  //mytime = millis() + interval;
-  //checkStavu();
+  mytime = millis() + interval;
+  checkStavu();
 }
 
 void loop()
 {
-  //Serial.println("Loop");
-  //inputUzivatel();
-  if (Serial.available() > 0)
-  {
-    //Serial.println("Check inputu");
-    interval = Serial.read()*10000;
-    Serial.flush();
-    pocetPomodor = 0;
-    checkStavu();
-  }
+  inputUzivatel();
 }
 
 void odpocet()
 {
-  //Serial.print("Zacatek odpoctu");
   while ( mytime >= millis() )
   {
     lcd.setCursor(1, 1);
@@ -52,73 +38,52 @@ void odpocet()
     minutes = counter / 60;
     counter -= (minutes * 60);
     seconds = counter;
-    Serial.print(hours);
     lcd.print(hours);
-    Serial.print(":");
     lcd.print(":");
     if (minutes < 10)
     {
-      Serial.print("0");
-      Serial.print(minutes);
       lcd.print("0");
       lcd.print(minutes);
     }
     else
-    {
-      Serial.print(minutes);
       lcd.print(minutes);
-    }
-    Serial.print(":");
     lcd.print(":");
     if (seconds < 10) {
-      Serial.print("0");
-      Serial.println(seconds);
       lcd.print("0");
       lcd.print(seconds);
     }
     else
-    {
-      Serial.println(seconds);
       lcd.print(seconds);
-    }
   }
   checkStavu();
 }
 
 void pomodoro()
 {
-  //Serial.println("pomodoro");
   lcd.clear();
   lcd.setCursor(0, 0);
-  Serial.println("Kolik zbyva: ");
   lcd.print("Kolik zbyva: ");
-  pocetPomodor++;
+  pocetPomodor = pocetPomodor + 1;
   odpocet();
 }
 
 void prestavka()
 {
-  //Serial.println("prestavka");
   lcd.clear();
   lcd.setCursor(0, 0);
-  Serial.println("Prestavka: ");
   lcd.print("Prestavka: ");
   odpocet();
 }
 
 void konec()
 {
-  //Serial.println("Konec");
   lcd.clear();
   lcd.setCursor(0, 0);
-  Serial.println("Autak je idiot");
   lcd.print("Autak je idiot");
-  cil = pocetPomodor + 3;
 }
 
 void checkStavu()
 {
-  //Serial.println("checkStavu");
   if (pocetPomodor >= cil)
   {
     konec();
@@ -135,5 +100,13 @@ void checkStavu()
     {
       prestavka();
     }
+  }
+}
+
+int inputUzivatel()
+{
+  if (Serial.avaible() > 0)
+  {
+    
   }
 }
